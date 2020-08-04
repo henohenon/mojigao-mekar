1b0759183f1ff934db52c62dc928a400a1eb7ef0
henohenon <takeru_20n1100821@nnn.ed.jp>
2020-08-04 16:37:02 +0900

    うーむ

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 'use strict';
 const http = require('http');
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
-  });
-  res.write(req.headers['user-agent']);
-  res.end();
+const auth = require('http-auth');
+const router = require('./lib/router');
+
+const basic = auth.basic({//ユーザー情報的な
+  realm: 'Enter username and password.',
+  file: './users.htpasswd'
+});
+
+
+
+const server = http.createServer((req, res) => {//サーバー建てるよぉーってかっ建ったとき(多分)
+	router.route(req, res);
+}).on('error', (e) => {//サーバーエラー出てまっせっていう
+	console.error('[' + new Date() + '] Server Error', e);
+}).on('clientError', (e) => {//クライアントエラー出てまっせ
+	console.error('[' + new Date() + '] Client Error', e);
 });
 const port = 8000;
 server.listen(port, () => {
-  console.log('Listening on ' + port);
+	console.info('[' + new Date() + '] Listening on ' + port);
 });
